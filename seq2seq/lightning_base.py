5c1fbd6fa1d2a805dff03e0d50e8bd920beec43f
sgallon <shcmsgallon@outlook.com>
2022-08-26 19:39:34 +0900

    change kwarg for ModelCheckpoint

--- seq2seq/lightning_base.py
+++ seq2seq/lightning_base.py
@@ -736,7 +736,8 @@ def generic_train(
 
     if checkpoint_callback is None:
         checkpoint_callback = pl.callbacks.ModelCheckpoint(
-            filepath=args.output_dir, prefix="checkpoint", monitor="val_loss", mode="min", save_top_k=1
+            # filepath=args.output_dir, prefix="checkpoint", monitor="val_loss", mode="min", save_top_k=1
+            dirpath = args.output_dir, monitor = "val_loss", mode = "min", save_top_k = 1
         )
     # add custom checkpoints
     # LISA
@@ -746,7 +747,7 @@ def generic_train(
     #     )
 
     #get_checkpoint_callback(args.output_dir, model.val_metric, args.save_top_k, lower_is_better)
-    checkpoint_callback = OurModelCheckPoint(filepath=args.output_dir, prefix="checkpoint", monitor="rouge2", mode="max", save_top_k=-1)
+    checkpoint_callback = OurModelCheckPoint(dirpath=args.output_dir, prefix="checkpoint", monitor="rouge2", mode="max", save_top_k=-1)
 
     # checkpoint_callback = OurModelCheckPoint(
     #     filepath=os.path.join(args.output_dir, exp),
