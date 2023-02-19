a08a0c86f319d86acf11c59cf09bccd9e6906f89
sgallon <shcmsgallon@outlook.com>
2023-02-19 13:50:48 +0900

    load prefix for train

--- seq2seq/train_bart.py
+++ seq2seq/train_bart.py
@@ -258,6 +258,7 @@ if __name__ == '__main__':
     if args.do_train == 'yes':
         COMMANDLINE = 'python finetune.py ' \
                       '--model_name_or_path {} ' \
+                      '--prefixModel_name_or_path {} ' \
                       '--output_dir {} ' \
                       '--data_dir {} ' \
                       '--tuning_mode {} ' \
@@ -269,7 +270,7 @@ if __name__ == '__main__':
                       '--learning_rate {} ' \
                       '--train_batch_size {} ' \
                       '--eval_batch_size {} ' \
-                      '--num_train_epochs {} '.format(OLD_MODEL, Model_FILE, data_dir, args.tuning_mode, args.preseqlen,
+                      '--num_train_epochs {} '.format(OLD_MODEL, args.prefix_model_path, Model_FILE, data_dir, args.tuning_mode, args.preseqlen,
                                                       args.label_smoothing, args.use_deep,
                                                       args.gpus, args.learning_rate, args.bsz, args.bsz, args.epoch)
     else:
